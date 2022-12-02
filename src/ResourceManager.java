ed0122e61022bc409e13eedec39f08b0c33b4879
mannin35 <mannin35@purdue.edu>
2022-12-01 20:33:48 -0500

    updated

--- src/ResourceManager.java
+++ src/ResourceManager.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+
 public class ResourceManager {
     private Object fileLock;
     private Object listLock;
     private ArrayList<?> list;
 
-    public ResourceManager(String filename)
+    public ResourceManager(String filename) {
+        
+    }
     
 }
