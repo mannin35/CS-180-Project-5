011088d2aa575f9b0b7d6afaac4ce1ba0462d848
mannin35 <mannin35@purdue.edu>
2022-12-01 21:51:58 -0500

    Merge branch 'main' of https://github.com/mannin35/CS-180-Project-5 into rei1

--- src/ResourceManager.java
+++ src/ResourceManager.java
@@ -1,8 +1,75 @@
-public class ResourceManager {
+import java.util.ArrayList;
+import java.io.*;
+
+public class ResourceManager<T> {
+    private String filename;
     private Object fileLock;
     private Object listLock;
-    private ArrayList<?> list;
+    private ArrayList<T> list;
 
-    public ResourceManager(String filename)
+    public ResourceManager(String filename) {
+        this.filename = filename;
+        this.list = new ArrayList<T>();
+        this.fileLock = new Object();
+        this.listLock = new Object();
+    }
     
+
+    public ResourceManager() {
+        this.list = new ArrayList<T>();
+        this.listLock = new Object();
+    }
+
+    public ArrayList<T> getList() {
+        synchronized(listLock) {
+            return list;
+        }
+    }
+    public T getAt(int index) {
+        synchronized(listLock) {
+            return list.get(index);
+        }
+    }
+
+    public void add(T element) {
+        synchronized(listLock) {
+            list.add(element);
+        }
+    }
+
+    public void appendToFile(String toAppend) {
+        synchronized(fileLock) {
+            try (PrintWriter writer = new PrintWriter(new FileWriter(filename, true))) {
+                writer.println(toAppend);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public ArrayList<String> readFile() {
+        // Create file (does nothing if it already exists)
+        synchronized(fileLock) {
+            File file = new File(filename);
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Read the lines of the given file
+            ArrayList<String> lines = new ArrayList<>();
+            String line;
+            try (BufferedReader bfr = new BufferedReader(new FileReader(file))) {
+                while ((line = bfr.readLine()) != null) {
+                    lines.add(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return lines;
+        }
+    }
 }
+
